a395677840668268e6a637c654b256c1ac5298f3
bstawy <125200508@sha.edu.eg>
2024-02-23 01:34:15 +0200

    Create application theme

--- lib/core/config/application_theme.dart
+++ lib/core/config/application_theme.dart
@@ -9,6 +9,44 @@ class ApplicationTheme {
       onBackground: const Color(0xff004182).withOpacity(0.3),
       secondary: const Color(0xff000000).withOpacity(0.7),
     ),
-    textTheme: TextTheme(),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Color(0xff004182),
+        fontFamily: "Poppins",
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+    fontFamily: "Poppins",
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: Colors.white,
+        fontSize: 24,
+        fontWeight: FontWeight.w600,
+      ),
+      titleMedium: TextStyle(
+        color: Color(0xff004182),
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+      titleSmall: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyLarge: TextStyle(
+        color: Color(0xff004182),
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      bodySmall: TextStyle(
+        color: Color(0xff004182),
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
+    ),
   );
 }
