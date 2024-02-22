a16d4a8657069c56a8ee7a694f78c4213b5a7e31
bstawy <125200508@sha.edu.eg>
2024-02-23 01:35:14 +0200

    Configure DI file for automatically injection

---
+++ lib/core/di/di.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'di.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit(
+  initializerName: 'init', // default
+  preferRelativeImports: true, // default
+  asExtension: true, // default
+)
+void configureDependencies() => getIt.init();
