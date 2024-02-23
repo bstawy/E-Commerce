efedc4afbdc4cdfbbfaf2da86d3fc1d66842f786
bstawy <125200508@sha.edu.eg>
2024-02-23 13:06:32 +0200

    Create main and configure screen util, injection dependencies and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+import 'core/config/app_router.dart';
+import 'core/config/application_theme.dart';
+import 'core/config/page_route_names.dart';
+import 'core/di/di.dart';
+
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+
+  configureDependencies();
   runApp(const MyApp());
 }
 
@@ -9,9 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'E-Commerce',
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'E-Commerce',
+          theme: ApplicationTheme.applicationTheme,
+          themeMode: ThemeMode.light,
+          navigatorKey: navigatorKey,
+          initialRoute: PageRouteNames.initial,
+          onGenerateRoute: AppRouter.onGeneratedRoute,
+        );
+      },
     );
   }
 }
