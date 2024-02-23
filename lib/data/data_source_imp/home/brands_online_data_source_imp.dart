696a13aca37d2efb814ca2a67978c80498451fef
bstawy <125200508@sha.edu.eg>
2024-02-23 13:27:22 +0200

    Create implementation for data source classes

---
+++ lib/data/data_source_imp/home/brands_online_data_source_imp.dart
@@ -0,0 +1,20 @@
+import 'package:injectable/injectable.dart';
+
+import '../../../core/web_services/api_manager.dart';
+import '../../../domain/entities/home/brand_entity.dart';
+import '../../data_source/home/brands_data_source.dart';
+
+@Injectable(as: BrandsDataSource)
+class OnlineBrandsDataSourceImp extends BrandsDataSource {
+  ApiManager apiManager;
+
+  @factoryMethod
+  OnlineBrandsDataSourceImp(this.apiManager);
+
+  @override
+  Future<List<Brand>?> getBrands() async {
+    var response = await apiManager.getBrands();
+
+    return response.data?.map((brandDto) => brandDto.toBrand()).toList();
+  }
+}
