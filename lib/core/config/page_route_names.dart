1ebbb7f0b62c2f2b1c70a242edde3a01b66c3565
bstawy <125200508@sha.edu.eg>
2024-02-24 22:14:19 +0200

    Change home screen route

--- lib/core/config/page_route_names.dart
+++ lib/core/config/page_route_names.dart
@@ -1,5 +1,5 @@
 class PageRouteNames {
-  static const String initial = "/"; // login is initial route
+  static const String initial = "/"; // login is application initial route
   static const String signUp = "signUp";
-  static const String home = "home";
+  static const String homeLayout = "layout";
 }
