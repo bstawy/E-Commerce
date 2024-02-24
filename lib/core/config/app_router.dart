405b4615fce66556ece1fc7ed09d5ddd6e449f04
bstawy <125200508@sha.edu.eg>
2024-02-24 20:50:40 +0200

    Generate home layout

--- lib/core/config/app_router.dart
+++ lib/core/config/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../features/home/pages/home_view.dart';
+import '../../features/home_layout/pages/layout.dart';
 import '../../features/login/pages/login_screen.dart';
 import '../../features/sign_up/pages/sign_up_screen.dart';
 import 'page_route_names.dart';
@@ -11,12 +11,15 @@ class AppRouter {
       case PageRouteNames.initial:
         return MaterialPageRoute(
             builder: (context) => const LoginScreen(), settings: settings);
+
       case PageRouteNames.signUp:
         return MaterialPageRoute(
             builder: (context) => const SignUpScreen(), settings: settings);
-      case PageRouteNames.home:
+
+      case PageRouteNames.homeLayout:
         return MaterialPageRoute(
-            builder: (context) => const HomeView(), settings: settings);
+            builder: (context) => Layout(), settings: settings);
+
       default:
         return MaterialPageRoute(
             builder: (context) => const LoginScreen(), settings: settings);
