3fb3c90ffdd19414e793b0c774153bf907a5008d
bstawy <125200508@sha.edu.eg>
2024-02-28 22:16:51 +0200

    Add headers and register and login end points

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,8 +1,13 @@
 class Constants {
-  static const String baseUrl = "https://ecommerce.routemisr.com";
+  static const String baseUrl = "ecommerce.routemisr.com";
+  static const Map<String, String> headers = {
+    "Content-Type": "application/json"
+  };
 }
 
 class EndPoints {
+  static const String register = "/api/v1/auth/signup";
+  static const String login = "/api/v1/auth/signin";
   static const String allCategories = "/api/v1/categories";
   static const String allBrands = "/api/v1/brands";
   static const String allProducts = "/api/v1/products";
