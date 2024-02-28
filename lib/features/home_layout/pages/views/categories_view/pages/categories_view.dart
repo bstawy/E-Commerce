313a75bb53742beacc2655961c2f968775d7ebe0
bstawy <125200508@sha.edu.eg>
2024-02-28 22:16:19 +0200

    Create categories view

---
+++ lib/features/home_layout/pages/views/categories_view/pages/categories_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
