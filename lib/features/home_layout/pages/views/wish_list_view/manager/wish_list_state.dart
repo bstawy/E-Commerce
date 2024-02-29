a3cf84c20425594da061bc0add9f081bf8d1cc49
bstawy <125200508@sha.edu.eg>
2024-02-29 20:40:25 +0200

    Create wishList cubit

--- lib/features/home_layout/pages/views/wish_list_view/manager/wish_list_state.dart
+++ lib/features/home_layout/pages/views/wish_list_view/manager/wish_list_state.dart
@@ -3,4 +3,20 @@ part of 'wish_list_cubit.dart';
 @immutable
 abstract class WishListState {}
 
-class UnAuthenticatedUserState extends WishListState {}
+class InitialState extends WishListState {}
+
+class UnLoggedUserState extends WishListState {}
+
+class LoadingState extends WishListState {}
+
+class SuccessState extends WishListState {
+  final List<Product> wishList;
+
+  SuccessState(this.wishList);
+}
+
+class FailureState extends WishListState {
+  final ServerFailure serverFailure;
+
+  FailureState(this.serverFailure);
+}
