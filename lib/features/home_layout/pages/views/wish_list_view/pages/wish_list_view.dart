92c2e711e3d612631759f1996493beabab7c3871
bstawy <125200508@sha.edu.eg>
2024-02-29 20:40:38 +0200

    Create wishList view

--- lib/features/home_layout/pages/views/wish_list_view/pages/wish_list_view.dart
+++ lib/features/home_layout/pages/views/wish_list_view/pages/wish_list_view.dart
@@ -1,25 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../../../core/di/di.dart';
 import '../../../../widgets/login_required_widget.dart';
 import '../manager/wish_list_cubit.dart';
 
 class WishListView extends StatelessWidget {
-  WishListView({super.key});
-
-  final WishListCubit wishListCubit = getIt<WishListCubit>();
+  const WishListView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WishListCubit wishListCubit = context.read<WishListCubit>();
+
     return BlocBuilder<WishListCubit, WishListState>(
-      bloc: wishListCubit,
+      bloc: wishListCubit..checkLogging(),
       builder: (context, state) {
-        if (state is UnAuthenticatedUserState) {
+        if (state is UnLoggedUserState) {
           return const LoginRequiredWidget(
               message: "Please login to see your WishList");
+        } else {
+          switch (state) {
+            case LoadingState():
+              return const Center(child: CircularProgressIndicator());
+            case SuccessState():
+              return ListView.builder(
+                itemCount: state.wishList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(state.wishList[index].title ?? ""),
+                    subtitle: Text(state.wishList[index].description ?? ""),
+                  );
+                },
+              );
+            case FailureState():
+              debugPrint("Exception: ${state.serverFailure}");
+              return Center(child: Text(state.serverFailure.message!));
+            default:
+              return const LoginRequiredWidget(
+                  message: "Please login to see your WishList");
+          }
         }
-        return Container();
       },
     );
   }
