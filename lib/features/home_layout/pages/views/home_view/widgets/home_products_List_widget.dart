eeef579667e52f92699369b58ecfcb1685753a8b
bstawy <125200508@sha.edu.eg>
2024-03-01 16:21:50 +0200

    Provide wish list cubit for products list view

--- lib/features/home_layout/pages/views/home_view/widgets/home_products_List_widget.dart
+++ lib/features/home_layout/pages/views/home_view/widgets/home_products_List_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../../core/di/di.dart';
 import '../../../../../../core/extensions/extensions.dart';
 import '../../../../../../domain/entities/home/product_entity.dart';
 import '../../../../../widgets/custom_product_widget.dart';
+import '../../wish_list_view/manager/wish_list_cubit.dart';
 
 class HomeProductsListWidget extends StatelessWidget {
   final List<Product>? products;
@@ -39,14 +42,17 @@ class HomeProductsListWidget extends StatelessWidget {
           ],
         ).setHorizontalPadding(context, 16.w),
         Expanded(
-          child: ListView.builder(
-            padding:
-                EdgeInsets.only(top: 16.h, left: 16.w, right: 16.w, bottom: 24),
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            itemCount: products?.length,
-            itemBuilder: (context, index) =>
-                CustomProductWidget(product: products![index]),
+          child: BlocProvider(
+            create: (context) => getIt<WishListCubit>()..checkLogging(),
+            child: ListView.builder(
+              padding: EdgeInsets.only(
+                  top: 16.h, left: 16.w, right: 16.w, bottom: 24),
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: products?.length,
+              itemBuilder: (context, index) =>
+                  CustomProductWidget(product: products![index]),
+            ),
           ),
         ),
       ],
