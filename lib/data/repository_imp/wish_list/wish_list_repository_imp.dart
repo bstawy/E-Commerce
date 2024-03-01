c0b1ad17f507f65527c478f8ef6f17dfb3504d36
bstawy <125200508@sha.edu.eg>
2024-03-01 16:23:30 +0200

    Create wish list repository

--- lib/data/repository_imp/wish_list/wish_list_repository_imp.dart
+++ lib/data/repository_imp/wish_list/wish_list_repository_imp.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../core/error/server_failure.dart';
+import '../../../domain/entities/wish_list/action_on_wish_list_entity.dart';
 import '../../../domain/entities/wish_list/wish_list_entity.dart';
 import '../../../domain/repository/wish_list/wish_list_repository.dart';
 import '../../data_source/wish_list/wish_list_data_source.dart';
@@ -16,5 +17,17 @@ class WishListRepositoryImp extends WishListRepository {
   @override
   Future<Either<ServerFailure, WishList?>> getWishList() {
     return wishListDataSource.getWishList();
+  }
+
+  @override
+  Future<Either<ServerFailure, ActionOnWishListModel?>>
+      removeProductFromWishList(String productId) {
+    return wishListDataSource.removeProductFromWishList(productId);
+  }
+
+  @override
+  Future<Either<ServerFailure, ActionOnWishListModel?>> addProductToWishList(
+      String productId) {
+    return wishListDataSource.addProductToWishList(productId);
   }
 }
