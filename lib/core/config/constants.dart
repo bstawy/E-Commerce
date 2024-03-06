317802bd6ac29f2d317f29657d25dfb2bf53053f
bstawy <125200508@sha.edu.eg>
2024-03-06 20:48:18 +0200

    Add sub categories end point in constants

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -6,6 +6,7 @@ class EndPoints {
   static const String register = "/api/v1/auth/signup";
   static const String login = "/api/v1/auth/signin";
   static const String allCategories = "/api/v1/categories";
+  static const String subCategoriesOnCategory = "/subcategories";
   static const String allBrands = "/api/v1/brands";
   static const String allProducts = "/api/v1/products";
   static const String wishList = "/api/v1/wishlist";
