8d1dfea9a2f955e101c1eafc37ba300559fcd107
bstawy <125200508@sha.edu.eg>
2024-03-08 16:47:50 +0200

    Create category products screen

---
+++ lib/features/category_products/category_products_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryProductsScreen extends StatelessWidget {
+  const CategoryProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
