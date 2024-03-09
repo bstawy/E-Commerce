db9bdddda77c42bab43afbe30afad4da93eb54f1
bstawy <125200508@sha.edu.eg>
2024-03-09 03:01:38 +0200

    Create category's products use case

---
+++ lib/domain/use_case/category_products_use_case.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/server_failure.dart';
+import '../entities/home/product_entity.dart';
+import '../repository/category_products_repository/category_products_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class CategoryProductsUseCase {
+  CategoryProductsRepository categoryProductsRepository;
+
+  @factoryMethod
+  CategoryProductsUseCase(this.categoryProductsRepository);
+
+  Future<Either<ServerFailure, List<Product>?>> execute(
+      String categoryId) async {
+    var response =
+        await categoryProductsRepository.getCategoryProducts(categoryId);
+    return response.fold((l) {
+      return Left(l);
+    }, (r) {
+      return Right(r);
+    });
+  }
+}
