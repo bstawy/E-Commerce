05b1e1f192d8abd8489bf5cb5cc4d22112282ed8
bstawy <125200508@sha.edu.eg>
2024-03-14 21:50:33 +0200

    Get cached category products data from local data source

--- lib/data/data_source_imp/category_products/category_products_local_data_source_impl.dart
+++ lib/data/data_source_imp/category_products/category_products_local_data_source_impl.dart
@@ -1,15 +1,24 @@
+import 'package:e_commerce/core/config/constants.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../../core/data_services/local_storage/hive_manager.dart';
 import '../../../domain/entities/home/product_entity.dart';
 import '../../data_source/category_products/category_products_local_data_source.dart';
 
 @Injectable(as: CategoryProductsLocalDataSource)
 class CategoryProductsLocalDataSourceImpl
     extends CategoryProductsLocalDataSource {
-  // TODO: open Box
+  HiveManager hiveManager;
+
+  CategoryProductsLocalDataSourceImpl(this.hiveManager);
+
   @override
   List<Product> getCategoryProducts(String categoryId) {
-    // TODO: implement getCategoryProducts
-    throw UnimplementedError();
+    var products = hiveManager
+        .retrieveData<Product>(HiveBoxKeys.categoryProducts)
+        .where((product) => product.subcategory!.contains(categoryId))
+        .toList();
+
+    return products;
   }
 }
