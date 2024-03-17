75b24e16d8492f51b18249f86040c3b84cccf817
bstawy <125200508@sha.edu.eg>
2024-03-17 14:09:19 +0200

    Delete CategoryProductsRepository interface
    Removed CategoryProductsRepository interface as it is no longer needed. This interface was previously used to fetch category products, but is no longer necessary for the current implementation.

--- lib/domain/repository/category_products_repository/category_products_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../core/error/failures.dart';
-import '../../entities/home/product_entity.dart';
-
-abstract class CategoryProductsRepository {
-  Future<Either<ServerFailure, List<Product>?>> getCategoryProducts(
-      String categoryId);
-}
